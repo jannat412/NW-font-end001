1ce502d6ee831f1eac55bf6b5563cf5376f8bfae
Moshiur Raihan <moshiurraihan95@gmail.com>
2017-11-09 17:30:51 +0600

    09-11-17

--- header.php
+++ header.php
@@ -23,6 +23,12 @@
     </head>
     <body>
         <header>
+            <div class="col-lg-12">
+                <p id="demo" class="text-center"></p>
+                <script>
+                    document.getElementById("demo").innerHTML = Date();
+                </script>
+            </div>
             <div class="container main-content">
                 <div class="row">
                     <div class="logo col-lg-6">
@@ -31,18 +37,14 @@
                         </a>
                     </div>
                     <div class="date col-lg-6">
-                        <p id="demo"></p>
-                        <script>
-                            document.getElementById("demo").innerHTML = Date();
-                        </script>
                         <div class="add">
-                            
+
                         </div>
                     </div>
                 </div>
             </div>
             <!--Navbar start here --->
-            <nav class="navbar navbar-default menu" data-spy = "affix" data-offset-top= "197">
+            <nav class="navbar menu" data-spy = "affix" data-offset-top= "197">
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="navbar-header">
@@ -55,30 +57,78 @@
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
+                    <div class="collapse navbar-collapse menu-item" id="bs-example-navbar-collapse-1">
+                        <ul class="nav nav-justified menu-item-content">
+                            <li>
+                                <a href="#" class="menu-items">নীড়পাতা</a>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">রাজনীতি</a>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">সারা বাংলা</a>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">অর্থনীতি</a>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">আন্তর্জাতিক</a>
+                            </li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">নীড়পাতা<i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li><a href="#">One more separated link</a></li>
+                                <a href="#" class="dropdown-toggle menu-items" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">খেলা<i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu dropdown-content">
+                                    <li><a href="#">ক্রিকেট</a></li>
+                                    <li><a href="#">ফুটবল</a></li>
+                                    <li><a href="#">হকি</a></li>
+                                    <li><a href="#">টেনিস</a></li>
                                 </ul>
                             </li>
-                            <li><a href="#">রাজনীতি</a></li>
-                            <li><a href="#">সারা বাংলা</a></li>
-                            <li><a href="#">অর্থনীতি</a></li>
-                            <li><a href="#">আন্তর্জাতিক</a></li>
-                            <li><a href="#">খেলা</a></li>
-                            <li><a href="#">বিনোদন</a></li>
-                            <li><a href="#">ফিচার</a></li>
-                            <li><a href="#">আজব নিউজ</a></li>
-                            <li><a href="#">কলাম</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle menu-items" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">বিনোদন<i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu dropdown-content">
+                                    <li><a href="#">ঢালিউড</a></li>
+                                    <li><a href="#">বলিউড</a></li>
+                                    <li><a href="#">হলিউড</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle menu-items" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ফিচার<i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu dropdown-content">
+                                    <li><a href="#">প্রবাস</a></li>
+                                    <li><a href="#">ফেসবুক কথন</a></li>
+                                    <li><a href="#">জীবনযাপন</a></li>
+                                    <li><a href="#">নারী</a></li>
+                                    <li><a href="#">সাহিত্য</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">আজব নিউজ</a>
+                            </li>
+                            <li>
+                                <a href="#" class="menu-items">কলাম</a>
+                            </li>
                         </ul>
                     </div><!-- /.navbar-collapse -->
                 </div><!-- /.container-fluid -->
+                <script type="text/javascript">
+                    $('.dropdown').hover(
+                            function () {
+                                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+                            },
+                            function () {
+                                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+                            }
+                    );
+
+                    $('.dropdown-menu').hover(
+                            function () {
+                                $(this).stop(true, true);
+                            },
+                            function () {
+                                $(this).stop(true, true).delay(200).fadeOut();
+                            }
+                    );
+                </script>
             </nav>
         </header>
 
---
+++ kolamist.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Kolamist Page</title>
+    <link rel="stylesheet" href="css/kolamist.css"/>
+</head>
+<body>
+    <?php require_once './header.php';?>
+    
+    
+    
+    <?php require_once './footer.php';?>
+</body>
+</html>
+
