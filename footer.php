98c60af58fcc28e6cd738af1585fa77f371082fc
Moshiur Raihan <moshiurraihan95@gmail.com>
2017-11-21 17:07:47 +0600

    21-11017

--- footer.php
+++ footer.php
@@ -1,73 +1,189 @@
 
-        <footer>
-            <div class="footer-part">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <ul>
-                                <li><a href="#">আমাদের সম্পর্কে</a></li>
-                                <li><a href="#">শর্তাবলী</a></li>
-                                <li><a href="#">বিজ্ঞাপন দিন</a></li>
-                                <li><a href="#">যোগাযোগ করুন</a></li>
+<footer>
+    <div class="footer-part">
+        <div class="container">
+            <div class="row">
+                <div class="footer_part_content">
+                    <div class="col-lg-4">
+                        <h5>সম্পাদকের বিস্তারিত</h5>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="newspaper_details">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">আমাদের সম্পর্কে</a>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form>
+                                                        <div class="form-group">
+                                                            <label for="recipient-name" class="control-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="message-text" class="control-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Send message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">শর্তাবলী</a>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form>
+                                                        <div class="form-group">
+                                                            <label for="recipient-name" class="control-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="message-text" class="control-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Send message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">বিজ্ঞাপন দিন</a>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form>
+                                                        <div class="form-group">
+                                                            <label for="recipient-name" class="control-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="message-text" class="control-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Send message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">যোগাযোগ করুন</a>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form>
+                                                        <div class="form-group">
+                                                            <label for="recipient-name" class="control-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="message-text" class="control-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Send message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
                             </ul>
                         </div>
-                        <div class="col-lg-4">
-                            <a href="#">
-                                <img src="images/Logo-Simanto.png" alt="Logo" class="img-responsive"/>
-                            </a>
-                        </div>
-                        <div class="col-lg-4">
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="address_content">
                             <div class="phn_no">
-                                <span><i class="fa fa-phone" aria-hidden="true"></i>0170 000 000</span>
+                                <span><i class="fa fa-phone icon" aria-hidden="true"></i>0170 000 000</span>
                             </div>
                             <div class="email_add">
-                                <span><i class="fa fa-envelope-o" aria-hidden="true"></i></i>sample@gmail.com</span>
+                                <span><i class="fa fa-envelope-o icon" aria-hidden="true"></i>sample@gmail.com</span>
                             </div>
                             <div class="address">
-                                <span><i class="fa fa-location-arrow" aria-hidden="true"></i>যাত্রাবাড়ী পানির ট্যাঙ্ক যাত্রাবাড়ী ঢাকা</span>
+                                <span><i class="fa fa-address-book icon" aria-hidden="true"></i>যাত্রাবাড়ী পানির ট্যাঙ্ক যাত্রাবাড়ী ঢাকা</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="footer-bottom">
-                <!--Back to top button-->
-                <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left">
-                    <span class="glyphicon glyphicon-chevron-up"></span>
-                </a>
-            </div>
-        </footer>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <!--Back to top button-->
+        <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="" data-toggle="tooltip" data-placement="left">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+    </div>
+</footer>
 
 
 
 
 
-        <!-- bootstarp min js link here-->
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- bootstarp min js link here-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-        <!--Back to top button js work-->
-        <script type="text/javascript">
-                                    $(document).ready(function () {
+<!--Back to top button js work-->
+<script type="text/javascript">
+    $(document).ready(function () {
 
 
-                                        $(window).scroll(function () {
-                                            if ($(this).scrollTop() > 100) {
-                                                $('#back-to-top').fadeIn();
-                                            } else {
-                                                $('#back-to-top').fadeOut();
-                                            }
-                                        });
-                                        // scroll body to 0px on click
-                                        $('#back-to-top').click(function () {
-                                            $('#back-to-top').tooltip('hide');
-                                            $('body,html').animate({
-                                                scrollTop: 0
-                                            }, 800);
-                                            return false;
-                                        });
-                                        $('#back-to-top').tooltip('show');
-                                    });
-        </script>
-    </body>
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        $('#back-to-top').tooltip('show');
+    });
+</script>
+</body>
 </html>
 
